4d771ad58b9a7344c0607f8e1c8b756886b85443
Gregor Vilkner <gregorvilkner@users.noreply.github.com>
2024-07-25 16:01:22 -0400

    Update main.js
    extract authenticator

--- src/Flic/main.js
+++ src/Flic/main.js
@@ -1,10 +1,4 @@
-const authenticator = {
-	"graphQlEndpoint": "https://xxx.xxx.thinkiq.net/graphql",
-	"clientId": "ThinkIQ.GraphQL.xxx",
-	"clientSecret": "xxx-xxx-xxx-xxx-xxx",
-	"role": "xxx_ro_group",
-	"userName": "ThinkIQ.GraphQL.xxx",
-};
+const authenticator = require('./smip').authenticator;
 
 var buttonManager = require("buttons");
 var http = require("http");
