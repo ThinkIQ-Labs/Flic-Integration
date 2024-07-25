01e73431cf9d3e98a2d8f7789731584b53fc6228
Gregor Vilkner <gregorvilkner@users.noreply.github.com>
2024-07-25 16:00:50 -0400

    Create smip.js

---
+++ src/Flic/smip.js
@@ -0,0 +1,7 @@
+exports.authenticator = {
+	"graphQlEndpoint": "https://xxx.xxx.thinkiq.net/graphql",
+	"clientId": "ThinkIQ.GraphQL.xxx",
+	"clientSecret": "xxx-xxx-xxx-xxx-xxx",
+	"role": "xxx_ro_group",
+	"userName": "ThinkIQ.GraphQL.xxx",
+};
